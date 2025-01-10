7c5ed94a1f0d718c0bfa201a1bf44c957ed5a068
Chi Thien Ngo <125285789+ThienNg0@users.noreply.github.com>
2025-01-10 19:35:30 +0700

    nav forgotpassword

---
+++ lib/presentation/auth/pages/forgot_password.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:ecommerce_app/common/widgets/button/basic_app_button.dart';
+import 'package:flutter/gestures.dart';
+import 'package:ecommerce_app/presentation/auth/pages/enter_password.dart';
+import 'package:ecommerce_app/common/helper/navigator/app_navigator.dart';
+import 'package:ecommerce_app/presentation/auth/pages/signup.dart';
+import 'package:ecommerce_app/common/widgets/appbar/app_bar.dart';
+
+class ForgotPasswordPage extends StatelessWidget {
+  const ForgotPasswordPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const BasicAppbar(
+        hideBack: true,
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            _forgotPasswordText(context),
+            const SizedBox(
+              height: 20,
+            ),
+            _emailField(context),
+            const SizedBox(
+              height: 20,
+            ),
+            _continueButton(context),
+           
+            
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _forgotPasswordText(BuildContext context) {
+    return Text(
+      'Forgot Password',
+      style: TextStyle(
+        fontSize: 32,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+
+  Widget _emailField(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: 'Email',
+      ),
+    );
+  }
+
+  Widget _continueButton(BuildContext context) {
+    return BasicAppButton(
+      onPressed: () {
+        AppNavigator.push(context, const EnterPasswordPage());
+      },
+      title: 'Continue',
+    );
+  }
+
+  
+}
